9bc650e399f1dbd204f2ba3e292f810323cecc2a
aaleshpatil22@gmail.com <aaleshpatil22@gmail.com>
2022-06-24 21:43:07 +0530

    New2

--- e-commerce/src/theme-layout/Shop/Shop.js
+++ e-commerce/src/theme-layout/Shop/Shop.js
@@ -1,34 +1,34 @@
-import React,{useState} from 'react'
+import React, { useState } from 'react'
 import Card from 'react-bootstrap/Card'
 import Button from 'react-bootstrap/Button'
 import ShopData from './ShopData'
 
 const Shop = () => {
 
-const[item,setItem]=useState(ShopData)
+    const [item, setItem] = useState(ShopData)
 
-const filterItem=(categItem)=>{
+    const filterItem = (categItem) => {
 
-    const updatedItems =ShopData.filter((curElem)=>{ 
-        return curElem.type===categItem
-    });
+        const updatedItems = ShopData.filter((curElem) => {
+            return curElem.type === categItem
+        });
 
-    setItem(updatedItems)
-}
+        setItem(updatedItems)
+    }
 
     return (
         <>
 
             <h1 className='mt-5 text-center main-heading'> Our Product</h1>
             <hr />
-
+            <h2 className='text-center'>Product Type</h2>
             <div className='menu-tabs container'>
                 <div className='menu-tab d-flex justify-content-around'>
-                    <button className='btn btn-warning' onClick={()=>filterItem("Black Kibal")}>Black Kibal</button>
-                    <button className='btn btn-warning'onClick={()=>filterItem("Salt")}>Salt</button>
-                    <button className='btn btn-warning'onClick={()=>filterItem("Sexbomb")}>Sexbomb</button>
-                    <button className='btn btn-warning'onClick={()=>filterItem("Yezyow")}>Yezyow</button>
-                    <button className='btn btn-warning'onClick={()=>setItem(ShopData)}>All</button>
+                    <button className='btn btn-warning' onClick={() => filterItem("Black Kibal")}>Black Kibal</button>
+                    <button className='btn btn-warning' onClick={() => filterItem("Salt")}>Salt</button>
+                    <button className='btn btn-warning' onClick={() => filterItem("Sexbomb")}>Sexbomb</button>
+                    <button className='btn btn-warning' onClick={() => filterItem("Yezyow")}>Yezyow</button>
+                    <button className='btn btn-warning' onClick={() => setItem(ShopData)}>All</button>
                 </div>
             </div>
             <br></br><br></br>
